d2f4620f00c6b623bf757e68a6b2f9eed9ed35a3
Alexandr Kuznecov <7515313@gmail.com>
2017-11-24 11:34:10 +0300

    my 1st change

---
+++ src/First.java
@@ -0,0 +1,7 @@
+public class First {
+    public static void main (String args[]) {
+        String St1="BY JAVA";
+        System.out.println("Hello! This is my first program");
+        System.out.println(St1);
+    }
+}
