f406b13c107a486e9eb48ec5511bfd28a6f33012
Alexandr Kuznecov <7515313@gmail.com>
2018-01-06 23:26:19 +0300

    t30v1.0

--- tasksFrom30/src/z30p2_1_2_3_4_5_6/Main.java
+++ tasksFrom30/src/z30p2_1_2_3_4_5_6/Main.java
@@ -1,7 +1,42 @@
 package z30p2_1_2_3_4_5_6;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
 public class Main {
     public static void main(String[] args) {
+
+        //<<<   1   >>>
+        LocalDate d1 = LocalDate.of(2020,6,25);
+        System.out.println(d1);
+
+        //<<<   2   >>>
+        d1=LocalDate.now();
+        LocalDate d2=d1.plusMonths(3);
+        System.out.println(d2);
+
+        //<<<   3   >>>
+        DateTimeFormatter formatter=DateTimeFormatter.ofPattern("dd.LL.Y");
+        System.out.println(formatter.format(d1));
+
+        //<<<   4   >>>
+        d1=LocalDate.parse("26-03-2014",DateTimeFormatter.ofPattern("dd-LL-yyyy"));
+        System.out.println(d1);
+
+        //<<<   5   >>>
+        d1=LocalDate.now();
+        d2=LocalDate.of(2020,6,25);
+        System.out.println("дней между:" + d1.until(d2, ChronoUnit.DAYS));
+
+        //<<<   6   >>>
+        d1=LocalDate.now();
+        d2=LocalDate.of(2020,6,25);
+        Long sec=(d1.until(d2, ChronoUnit.DAYS))*24*60*60;
+        System.out.println("Секунд между:" + sec);
+
+
+
 
 
 
--- tasksFrom30/src/z30p2_7_8/Main.java
+++ tasksFrom30/src/z30p2_7_8/Main.java
@@ -1,10 +1,52 @@
 package z30p2_7_8;
 
-public class Main {
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.Temporal;
+import java.time.temporal.TemporalAdjuster;
+
+
+public class Main implements TemporalAdjuster {
     public static void main(String[] args) {
 
+        //for task 7
+        /*LocalDate d1 = LocalDate.now();
+        System.out.println(d1);
 
 
+        d1=d1.with(new Main());
+        System.out.println(d1);
+*/
+
+        LocalDate d1 = LocalDate.of(2005, 7, 2);
+        //LocalDate d1 = LocalDate.of(2005, 11, 5);
+        System.out.println(d1);
+        d1 = d1.with(new Main());
+        System.out.println(d1);
+
+
+    }
+
+
+    @Override
+    public Temporal adjustInto(Temporal temporal) {
+
+        LocalDate temp1 = LocalDate.from(temporal);
+        int i = temp1.getYear();
+
+        if ((temp1.lengthOfYear() - temp1.getDayOfYear()) < (temp1.lengthOfYear() / 2)) {
+            i++;
+        } else if ((temp1.lengthOfYear() - temp1.getDayOfYear()) == (temp1.lengthOfYear() / 2)) {
+            System.out.println("середина года");
+            i++;
+        }
+
+        temp1 = LocalDate.of(i, 1, 1);
+
+        return temp1;
+
+        //for task 7!!!
+        //return temporal.plus(42, ChronoUnit.DAYS);
 
     }
 }
